f27aee58c52f4665cb6c0bf867f31d4df3781ebc
yuribz <ybukhrad@ucsd.edu>
2024-06-26 14:32:51 -0700

    Wrong SDL2 version

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 all:
-	g++ -Isrc/include -Lsrc/lib -o snake src/snake.c -lmingw32 -lSDL2main -lSDL2
+	gcc -Isrc/include -Lsrc/lib -o snake src/snake.c -lmingw32 -lSDL2main -lSDL2
--- src/snake.c
+++ src/snake.c
@@ -1,38 +1,19 @@
-#include <SDL.h>
+#include <SDL2/SDL.h>
 #include <stdio.h>
 
 #define SCREEN_WIDTH 480
 #define SCREEN_HEIGHT 360
 
-int main()
+int main(int argc, char **arg)
 {
-    if(SDL_Init(SDL_INIT_VIDEO) < 0)
-    {
-        printf("Failed to initialize the SDL2 library\n");
-        return -1;
-    }
-
-    SDL_Window *window = SDL_CreateWindow("SDL2 Window",
-                                          SDL_WINDOWPOS_CENTERED,
-                                          SDL_WINDOWPOS_CENTERED,
-                                          680, 480,
-                                          0);
-
-    if(!window)
-    {
-        printf("Failed to create window\n");
-        return -1;
-    }
-
-    SDL_Surface *window_surface = SDL_GetWindowSurface(window);
-
-    if(!window_surface)
-    {
-        printf("Failed to get the surface from the window\n");
-        return -1;
-    }
-
-    SDL_UpdateWindowSurface(window);
-
-    SDL_Delay(5000);
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window *win = SDL_CreateWindow(
+        "title",
+        30, 30,
+        SCREEN_WIDTH, SCREEN_HEIGHT,
+        SDL_WINDOW_SHOWN);
+    SDL_Surface *screen = SDL_GetWindowSurface(win);
+    SDL_Delay(2000);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
 }
